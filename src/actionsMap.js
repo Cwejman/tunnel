5bedb52f7cac869347ecf405dbeee306d43317f0
Jona <jcwejman@gmail.com>
2020-12-28 22:43:34 +0100

    Fix: Gesture rotation speed

--- src/actionsMap.js
+++ src/actionsMap.js
@@ -18,10 +18,10 @@ export const gestureMove = (e) => R.evolve({
       const initDistance = controls.initDistance || distance;
 
       const { x: dx, y: dy } = M.subPoints(initCenter, center);
-      const yaw = M.clamp(-1, 1, dx / 40);
-      const pitch = M.clamp(-1, 1, dy / 40);
-      const roll = M.clamp(-1, 1, (rot - initRot) / 200);
-      const speed = M.clamp(0.5, 2, 1 + (distance - initDistance) / 100);
+      const yaw = M.clamp(-1, 1, dx / 40) * 0.02;
+      const pitch = M.clamp(-1, 1, dy / 40) * 0.02;
+      const roll = M.clamp(-1, 1, (rot - initRot) / 200) * 0.01;
+      const speed = M.clamp(0.5, 2, 1 + (distance - initDistance) / 100) * 0.03;
 
       return { initCenter, initRot, initDistance, yaw, pitch, roll, speed, active: true };
     } else {
